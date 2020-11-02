d25b2a3c7d56d5e53030999d1007b601dd5958a7
aditi3512 <73767726+aditi3512@users.noreply.github.com>
2020-11-02 15:33:56 +0800

    Update xbar_bypass.sv

--- xbar_bypass.sv
+++ xbar_bypass.sv
@@ -15,7 +15,7 @@
 ////                                                                        ////
 ////////////////////////////////////////////////////////////////////////////////
 ////                                                                        ////
-//// Copyright <YEAR> <COPYRIGHT HOLDER>				    ////
+//// Copyright 2020 Author(s)						    ////
 //// Permission is hereby granted, free of charge, to any person 	    ////
 //// obtaining a copy of this software and associated documentation 	    ////
 //// files (the "Software"), to deal in the Software without restriction,   ////
